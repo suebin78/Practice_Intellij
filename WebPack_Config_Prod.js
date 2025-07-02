75fef73a55b26cf87284157d171f50824b9c7415
suebin78 <ksb060708@hanyang.ac.kr>
2025-07-02 16:31:24 +0900

    add commit

--- WebPack_Config_Prod.js
+++ WebPack_Config_Prod.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const common = require('./WebPack_Common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
