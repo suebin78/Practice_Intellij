b6964887b885cdb65fa45cdd93f5d036a26349e6
suebin78 <ksb060708@hanyang.ac.kr>
2025-07-02 16:12:59 +0900

    add commit

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: './web.html',
     }),
     new CopyPlugin({
       patterns: [
